8c4c9d4429f7856bafbf758c97e35404a86766e0
kakizawaayato <chibairodori25@gmail.com>
2023-05-21 12:17:43 +0900

    🎨UI整理

--- src/components/home/homeTemplate.tsx
+++ src/components/home/homeTemplate.tsx
@@ -1,10 +1,15 @@
-import { Suspense } from 'react';
+import { Counter } from './counter';
 import { UserTodos } from './userTodos';
+import { Suspense } from 'react';
 
 export const HomeTemplate: React.FC = (): JSX.Element => {
   return (
-    <div>
-      <h1>TODOリスト</h1>
+    <div className="">
+      <div className="mb-8">
+        <h2>グローバルカウンター</h2>
+        <Counter />
+      </div>
+      <h2>TODOリスト</h2>
       <Suspense fallback={<>ローディング中。。</>}>
         {/* @ts-expect-error Server Component */}
         <UserTodos />
--- src/components/home/userTodo.tsx
+++ src/components/home/userTodo.tsx
@@ -15,7 +15,7 @@ export const UserTodo: FC<Props> = (props: Props): JSX.Element => {
   const todo = useTodo<Todo>(props.id, 1);
 
   return (
-    <div className="flex container mx-auto">
+    <div className="pl-4">
       <div>{todo != null ? `${todo.id}: ${todo.title}` : ''}</div>
     </div>
   );
