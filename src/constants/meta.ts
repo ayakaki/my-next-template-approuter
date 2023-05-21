58684afed1b6bf4faed01de594623402265465c6
kakizawaayato <chibairodori25@gmail.com>
2023-05-21 16:44:42 +0900

    ✨メタ情報追加

--- src/constants/meta.ts
+++ src/constants/meta.ts
@@ -1,5 +1,12 @@
 export const SITE_TITEL = 'My Next Tempalte';
 
-export const PAGE_TITLE = {
-  HOME: 'Home',
+export const PAGE_METAS = {
+  HOME: {
+    TITLE: 'Home',
+    DESCRIPTION: 'This is home page',
+  },
+  ABOUT: {
+    TITLE: 'About',
+    DESCRIPTION: 'This is about page',
+  },
 };
