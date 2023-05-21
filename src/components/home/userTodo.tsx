95be5a786059f7785f93ea9dbb7ad00e6ae0d70e
kakizawaayato <chibairodori25@gmail.com>
2023-05-21 17:08:22 +0900

    📝不要なコメント削除

--- src/components/home/userTodo.tsx
+++ src/components/home/userTodo.tsx
@@ -9,9 +9,6 @@ type Props = {
 };
 
 export const UserTodo: FC<Props> = (props: Props): JSX.Element => {
-  // TODO 状態管理
-  // const favoriteCount: number = useRecoilValue(favoriteCountState);
-
   const todo = useTodo<Todo>(props.id, 1);
 
   return (
